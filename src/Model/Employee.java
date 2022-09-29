954a19c4fd0ee1f52ee3e4b7e06ab2676daf2f5f
itzRishabh <pandey.risha@northeastern.edu>
2022-09-29 19:02:04 -0400

    Initial Commit

---
+++ src/Model/Employee.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author 91983
+ */
+public class Employee {
+    
+    private String name;
+    private int employee_id;
+    private int age;
+    private String date;
+    private int level;
+    private String team_info;
+    private String position_title;
+    private int cell_no;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getTeam_info() {
+        return team_info;
+    }
+
+    public void setTeam_info(String team_info) {
+        this.team_info = team_info;
+    }
+
+    public String getPosition_title() {
+        return position_title;
+    }
+
+    public void setPosition_title(String position_title) {
+        this.position_title = position_title;
+    }
+
+    public int getCell_no() {
+        return cell_no;
+    }
+
+    public void setCell_no(int cell_no) {
+        this.cell_no = cell_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+            
+    
+}
